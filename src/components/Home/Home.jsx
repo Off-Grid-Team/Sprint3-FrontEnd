9cd97cdf2c4c2326f764b8c2ca89b23d46107057
Felipe Rabelo <feliperabelo2010@gmail.com>
2026-08-25 16:35:47 -0300

    [Feat]: Adicionando imagens e link do protótipo.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,21 +4,28 @@ import './Home.css'
 const Home = () => {
   return (
     <>
-        <h1>Uma nova experiência para câmeras de celular</h1>
-        <h2>Reestruturação da interface do aplicativo com foco em uma experiência mais intuitiva, acessível e eficiente.</h2>
-        <p>Este projeto apresenta uma proposta de reestruturação da UI do aplicativo de câmera nativo dos celulares da JOVI, 
-          buscando solucionar problemas encontrados na interface atual e tornar a navegação mais simples e agradável para seus usuários.
-        </p>
+      <h1>Uma nova experiência para câmeras de celular</h1>
+      <h2>Reestruturação da interface do aplicativo com foco em uma experiência mais intuitiva, acessível e eficiente.</h2>
+      <p>Este projeto apresenta uma proposta de reestruturação da UI do aplicativo de câmera nativo dos celulares da JOVI, 
+        buscando solucionar problemas encontrados na interface atual e tornar a navegação mais simples e agradável para seus usuários.
+      </p>
 
-        <h2>O Problema</h2>
-        <p>Atualmente, muitos usuários enfrentam dificuldades ao utilizar as câmeras de seus dispositivos celulares,
-           principalmente na questão de intuitividade da interface, além da falta de explicação clara sobre as funcionalidades 
-           disponíveis. Isso pode levar a frustração e uma experiência de usuário insatisfatória.
-        </p>
-        
-      <button id="home-button" a="#">
-        Conheça o projeto!
-      </button>
+      <h2>O Problema</h2>
+      <p>Atualmente, muitos usuários enfrentam dificuldades ao utilizar as câmeras de seus dispositivos celulares,
+          principalmente na questão de intuitividade da interface, além da falta de explicação clara sobre as funcionalidades 
+          disponíveis. Isso pode levar a frustração e uma experiência de usuário insatisfatória.
+      </p>
+      <p>
+        Por isso, a proposta deste projeto é apresentar uma nova interface para o aplicativo de câmera nativo da JOVI,
+        com o objetivo de melhorar a experiência do usuário, tornando-a mais intuitiva, acessível e eficiente.
+      </p>
+      
+      <a className="button-container" href="#publico">
+        <button a href="#publico">
+          Conheça o projeto!
+        </button>
+      </a>
+      
     </>
   )
 }
